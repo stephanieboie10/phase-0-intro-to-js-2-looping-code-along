7782923346582ece9db5d84d46547656de1a573b
Stephanie Boie <stephanieboie10@gmail.com>
2023-03-06 17:18:26 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { getModulesPluginNames } = require("@babel/preset-env")
+
 // Code your solutions in this file
+function writeCards(names, value) {
+    let messageNames = []
+    for ( let i = 0; i < names.length; i++ ) {
+       messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`)
+    }
+    return messageNames
+}
+writeCards(["Guadalupe", "Ollie", "Aki", "surprise"])
+
+function countDown(number) {
+    let value = number
+    while (value >= 0) {
+        console.log(value--)
+    }
+    return number
+}
